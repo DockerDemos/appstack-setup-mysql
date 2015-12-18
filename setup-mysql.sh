b55a63d20f87e8dbacaf9de210c7738274f77028
Chris Collins <collins.christopher@gmail.com>
2015-12-18 16:06:50 -0500

    trying to fix deps

--- setup-mysql.sh
+++ setup-mysql.sh
@@ -11,7 +11,6 @@ ROOT_PASS=$(pwgen -c -n -1 16)
 BACKUP_PASS=$(pwgen -c -n -1 16)
 SECRET_DIR='/conf/.creds'
 DB_FILE="${SECRET_DIR}/dbdata.yaml"
-LOGFILE='/var/log/mariadb-setup.log'
 
 ##############################
 ### MYSQL CONFIG VARIABLES ###
@@ -26,39 +25,37 @@ MYSQL_SLO_LOGFILE="${MYSQL_LOGDIR}/mariadb-slow.log"
 ### FUNCTIONS ###
 
 f_err() {
-  echo "$(date '+%Y-%m-%dT%H:%M') - ERROR: $1" | tee $LOGFILE
+  echo "$(date '+%Y-%m-%dT%H:%M') - ERROR: $1" 
   exit 1
 }
 
 f_warn() {
-  echo "$(date '+%Y-%m-%dT%H:%M') - $1" | tee $LOGFILE
+  echo "$(date '+%Y-%m-%dT%H:%M') - WARN: $1" 
+  exit 0
+}
+
+f_notify() {
+  echo "$(date '+%Y-%m-%dT%H:%M') - $1" 
 }
 
 ### LOGFILE AND TESTS ###
 
-if [[ ! -f $LOGFILE ]] ; then
-  touch $LOGFILE || f_warn "Unable to open ${LOGFILE}"
-  echo "$(date '+%Y-%m-%dT%H:%M') - Logfile opened" >> $LOGFILE
-fi
-
-if [[ -z $ROOT_PASS ]] ; then
-  f_err "No root user password was generated - is pwgen installed?"
-fi
-
-if [[ -z $BACKUP_PASS ]] ; then
-  f_err "No backup password was generated - is pwgen installed?"
-fi
+for file in $ROOT_PASS $BACKUP_PASS $DB_NAME ; do
+  if [[ -z $file ]] ;
+    then f_err "No password were generated - is pwgen installed?"
+  fi
+done
 
 if [[ ! -d '/conf' ]] ; then
   f_err "There are no volumes mounted from the data container"
 fi
 
 if [[ -f $DB_FILE ]] ; then
-  f_err "A ${DB_FILE} already exists"
+  f_warn "A ${DB_FILE} already exists"
 fi
 
 if [ -f "${DATADIR}/ibdata1" ] ; then
-  f_err "${DATADIR}/ibdata1 file exists"
+  f_warn "${DATADIR}/ibdata1 file exists"
 fi
 
 #################################
@@ -66,7 +63,7 @@ fi
 #################################
 
 if [[ ! -d $SECRET_DIR ]] ; then
-  f_warn "$SECRET_DIR did not exist; creating"
+  f_notify "$SECRET_DIR did not exist; creating"
   mkdir -p $SECRET_DIR
 fi
 
@@ -81,7 +78,7 @@ else
   f_warn "A ${DB_FILE} already exists"
 fi
 
-chmod 600 $DB_FILE || f_warn "Unable to chown ${DB_FILE}"
+chmod 600 $DB_FILE || f_err "Unable to chown ${DB_FILE}"
 
 ##############################
 ### MYSQL SETUP BELOW HERE ###
@@ -90,7 +87,7 @@ chmod 600 $DB_FILE || f_warn "Unable to chown ${DB_FILE}"
 mkdir -p $MYSQL_LOGDIR || f_err "Unable to create log directory"
 
 for file in $MYSQL_ERR_LOGFILE $MYSQL_SLO_LOGFILE $MYSQL_LOGFILE ; do
-  touch $file | tee $LOGFILE || f_err "Unable to create ${file}"
+  touch $file || f_err "Unable to create ${file}"
   chown mysql:mysql $file || f_err "Unable to chown ${file} to mysql.mysql"
   chmod 0640 $file || f_err "Unable to chmod ${file} to 0640"
 done
